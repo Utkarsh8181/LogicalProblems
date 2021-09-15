70ca8ced1eb9e2929da2759df7cd913b4ad58373
Utkarsh Mishra <utkarshkr945@gmail.com>
2021-09-15 18:38:09 +0530

    Added: Stimulate Stopwatch

---
+++ src/com/bridgelabz/StimulateStopWatch.java
@@ -0,0 +1,24 @@
+package com.bridgelabz;
+import java.util.Scanner;
+
+public class StimulateStopWatch {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        /*
+         * @start - To start the timer
+         * @stop - To stop the timer
+         */
+        System.out.println("Enter 1 to start the clock");
+        double start = sc.nextDouble();
+        start = System.currentTimeMillis();
+        System.out.println("Enter 0 to stop the clock");
+        double stop = sc.nextDouble();
+        stop = System.currentTimeMillis();
+
+        // Converting to seconds and displaying.
+        double elapsed_time = (stop - start) / 1000;
+        System.out.println("Elapsed time is " + elapsed_time + "seconds");
+        sc.close();
+    }
+}
